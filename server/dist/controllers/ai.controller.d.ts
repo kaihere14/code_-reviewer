6d273c8d2ed441d291a8f91093f943b5726c2561
kaihere14 <yashpalsharma9950@gmail.com>
2026-01-05 22:35:16 +0530

    fix: remove unused type definition and source map files; restore '@types/node' entry in package-lock.json

--- server/dist/controllers/ai.controller.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from "express";
-export declare const generateAIContent: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=ai.controller.d.ts.map
--- server/dist/middlewares/verifyJwt.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-export declare const verifyJwt: (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=verifyJwt.d.ts.map
